361ec3c850eaa7916f0a8f7568fece8e4faa5a8e
smixs <smixs@users.noreply.github.com>
2026-09-16 16:20:10 +0500

    Claimp 0.3.2

--- Casks/claimp.rb
+++ Casks/claimp.rb
@@ -1,6 +1,6 @@
 cask "claimp" do
-  version "0.3.1"
-  sha256 "21ad6e2e8db1b7ec6492739bade579feac40c7ecdcbb746801a0d399258d13c9"
+  version "0.3.2"
+  sha256 "ef9ee3ef3a2de1f811f44758d3a8a734aaddc3d19bc5f565af24e3c221823e4d"
 
   url "https://github.com/smixs/claimp/releases/download/v#{version}/Claimp-#{version}.dmg"
   name "Claimp"
